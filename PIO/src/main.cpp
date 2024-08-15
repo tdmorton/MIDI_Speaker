d93f0c79375faa19fe77b96e00f99b67c75848c9
tdmorton <tdmorton22@gmail.com>
2024-08-14 23:41:11 -0400

    updated main.cpp to print to console

--- PIO/src/main.cpp
+++ PIO/src/main.cpp
@@ -2,14 +2,17 @@
 
 // put function declarations here:
 int myFunction(int, int);
+int result = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println(result);
 }
 
 // put function definitions here:
